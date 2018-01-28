804108f8ad96c3ceab18ed2643d3c594d2144047
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-28 22:57:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function iterativeLog(array){
   array.forEach((element,index) => console.log(`${index}: ${element}`));
 }
+
+function iterate(callback){
+  array = [1,2,3];
+  array.forEach(callback);
+  return array;
+}
